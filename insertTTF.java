d114204254e5125069a4c727fce2f80df54e5dbe
besweens <144698652+besweens@users.noreply.github.com>
2023-11-13 14:52:22 -0500

    beginning the insertion method

--- insertTTF.java
+++ insertTTF.java
@@ -1,4 +1,44 @@
 package Lab8;
 
-public class insertTTF {
+/**
+ * insertTTF extends TwoThreeFourTree to structure the insert, contains, and sorted
+ */
+public class insertTTF<E extends Comparable<E>> extends TwoThreeFourTree<E>{
+
+    /**
+     * Public-facing insertion method for new data.
+     * Will call the private method with the root as a localRoot
+     * @param data Data input for the tree
+     */
+    @Override
+    public void insert(E data) {
+        insert((E) this.getRoot(), data);
+    }
+
+    /**
+     * Private-facing insertion method for recursive insertion
+     *
+     * @param localRoot working root
+     * @param data data to insert
+     */
+    private void insert(E localRoot, E data){
+        if (this.getRoot() == null){
+            this.setRoot(new Node<E>(2));
+            this.getRoot().data[0] = data;
+        }
+        else{
+
+        }
+    }
+
+    @Override
+    public boolean contains(E data) {
+        return false;
+    }
+
+    @Override
+    public E[] getSorted() {
+        //return new E[];
+        return null;
+    }
 }
