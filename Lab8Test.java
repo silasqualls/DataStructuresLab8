f42bd262734d1ebf408a01ba30056cf01062ce7f
besweens <144698652+besweens@users.noreply.github.com>
2023-11-13 14:10:41 -0500

    initial class file push

---
+++ Lab8Test.java
@@ -0,0 +1,9 @@
+package Lab8;
+
+import java.io.IOException;
+
+public class Lab8Test {
+    public static void main(String[] args) throws IOException {
+        System.out.println("Lab8 Output test");
+    }
+}
